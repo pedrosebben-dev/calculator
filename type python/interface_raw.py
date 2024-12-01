e0795b1a8c85209258013f422cb92b31fce820f3
pedro sebben <dev.pedro_sebben@outlook.com.br>
2024-12-01 19:06:24 -0300

    Add files via upload

---
+++ type python/interface_raw.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+class NotificationSender(ABC):
+
+    @abstractmethod
+    def send_notification(self, message: str) -> None: pass
+
+class EmailNotificationSender(NotificationSender):
+
+    def send_notification(self, message: str) -> None:
+        print(f"Email message - {message}")
+
+class SMSNotificationSender(NotificationSender):
+
+    def send_notification(self, message: str) -> None:
+        print(f"SMS message - {message}")
+
+
+class Notificator:
+    def __init__(self, notification_sender: NotificationSender) -> None:
+        self._notification_sender = notification_sender
+
+    def send(self, message: str) -> None:
+        self.__notification_sender.send_notification(message)
+
+obj = Notificator(EmailNotificationSender())
+obj.send("olá Mundo")
+    
